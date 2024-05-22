d61a890df10f3ce5532789863441aaf123c3a057
NIRAJ <nirarajput6983@gmail.com>
2024-05-22 22:32:27 +0530

    22-05-2024

--- lib/Task_1/views/Homepage.dart
+++ lib/Task_1/views/Homepage.dart
@@ -1,3 +1,4 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 
 class Networkscreen extends StatelessWidget {
@@ -6,35 +7,38 @@ class Networkscreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
         backgroundColor: Colors.black,
-        title: Text(
-          'Connectivity',
-          style: TextStyle(
-              color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-        ),
-      ),
-      body: Column(
-        children: [
-          Center(
-            child: Container(
-              height: 280,
-              width: 300,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                  color: Colors.grey),
-            ),
+        appBar: AppBar(centerTitle: true,
+          backgroundColor: Colors.black,
+          title: Text(
+            'Connectivity',
+            style: TextStyle(
+                color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
           ),
-        ],
-      ),bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.person),label: 'Person',backgroundColor: Colors.black),
-            BottomNavigationBarItem(icon: Icon(Icons.call),label: 'Contact',backgroundColor: Colors.black),
-            BottomNavigationBarItem(icon: Icon(Icons.camera),label: 'Camera',backgroundColor: Colors.black),
-            BottomNavigationBarItem(icon: Icon(Icons.message),label: 'Message',backgroundColor: Colors.black),
-          ],
-    ),
+        ),
+        body: StreamBuilder(stream: Connectivity().onConnectivityChanged,
+            builder: (context,snapshot) {
+                 if(snapshot.data!.contains(ConnectivityResult.mobile) || snapshot.data!.contains(ConnectivityResult.wifi))
+                   {
+                     return Center(
+                     child: Image.asset('assets/the-loser-is-online-tb21j0uhxfhscy15.gif'),
+                     );
+                   }
+                 else{
+                   return Center(
+                   child: Image.asset('assets/download.png'),
+                   );
+                   bottomNavigationBar: BottomNavigationBar(
+                     items: [
+                       BottomNavigationBarItem(icon: Icon(Icons.person),label: 'Person',backgroundColor: Colors.black),
+                       BottomNavigationBarItem(icon: Icon(Icons.call),label: 'Contact',backgroundColor: Colors.black),
+                       BottomNavigationBarItem(icon: Icon(Icons.camera),label: 'Camera',backgroundColor: Colors.black),
+                       BottomNavigationBarItem(icon: Icon(Icons.message),label: 'Message',backgroundColor: Colors.black),
+                     ],
+                   );
+                }
+             }
+      )
     );
   }
 }
